f153f5024a4d31ac10d2bb85a6a00987569709a7
Le Fishe <amandapostma72@gmail.com>
2024-04-20 12:56:43 -0700

    update2

--- scripts/loop.js
+++ scripts/loop.js
@@ -1,8 +1,8 @@
-var words = ["The Shape", "Orb", "Creature", "Beast", "Little Man", "Amorphous Blob"];
+var words = ["The Shape, ", "Orb, ", "Creature, ", "Beast, ", "Little Man, ", "Amorphous Blob."];
 var msg = 'Words Used to describe Kosmo: ';
 
 for (i = 0; i < words.length; i++) {
-	msg += words[i] + ', ';
+	msg += words[i];
 }
 
 var el = document.getElementById('words');
