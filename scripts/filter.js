0a24b2cd6f67a9491560eba9d22e02ee50cb1113
Le Fishe <amandapostma72@gmail.com>
2024-05-27 14:44:00 -0700

    fix

--- scripts/filter.js
+++ scripts/filter.js
@@ -38,7 +38,7 @@ var btn = btnCon.getElementsByClassName("btn");
 
 for (var i = 0; i < btn.length; i++) {
 	btn[i].addEventListener("click", function(){
-		var current = documen.getElementsByClassName("active");
+		var current = document.getElementsByClassName("active");
 	current[0].className = current[0].className.replace(" active", "");
 	this.className += " active";
 	});
